2d8e2734a10f4df8f17ae660dec42216e10a2941
Justin Carter <jscarter@umich.edu>
2017-06-21 15:45:24 -0700

    Finished putting the translation back into the text area

--- main.py
+++ main.py
@@ -4,48 +4,47 @@ from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!DOCTYPE html>
-
-<html>
-    <head>
-        <style>
+encrypt_form = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <style>
             form {{
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
+                 background-color: #eee;
+                 padding: 20px;
+                 margin: 0 auto;
+                 width: 540px;
+                 font: 16px sans-serif;
+                 border-radius: 10px;
             }}
             textarea {{
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
+                 margin: 10px 0;
+                 width: 540px;
+                 height: 120px;
             }}
-        </style>
-    </head>
+            </style>
+        </head>
     <body>
-        <form method="post">
+        <form method="POST">
         <label for="rot"> Rotate by: </label>
         <input type="text" name="rot" value=0 id="rot">
         <input type="submit" value="Encrypt my text!">
-        <textarea name="text" id="text" value=''> </textarea>
+        <textarea name="text" id="text"> {encrypted_text} </textarea>
     </body>
-</html>
+ </html>
 """
 
 @app.route("/", methods=['POST'])
 def encrypt():
-    rot = request.args.get['rot']
-    text = request.args.get['text']
+    rot = request.form['rot']
+    text = request.form['text']
 
-    rot = int(rot)   
-    
-    return form.format(text='rotate_string(text, rot)')
+    rot = int(rot)
+    text = rotate_string(text, rot)
+    return encrypt_form.format(encrypted_text=text)
 
 @app.route("/",)
 def index():
-    return form.format()
+    return encrypt_form.format(encrypted_text='')
 
 app.run()
